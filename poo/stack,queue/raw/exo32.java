9595ee1c10e614def338953af59a00b88e6b32b4
Nicolas R <87765071+namrenard@users.noreply.github.com>
2022-03-23 00:58:46 +0100

    commit raw

--- poo/stack,queue/raw/exo32.java
+++ poo/stack,queue/raw/exo32.java
@@ -11,13 +11,46 @@ public class exo32 {
 	public static void expressionPolonaise() {
 		Scanner console = new Scanner(System.in);
 		System.out.println("Veuillez entrer une expression arithmétique en notation RPN");
-		System.out.println("notation RPN exemple : (2*(1+2) va donner 212*+ ");
-		int reponse = console.nextInt();
-		try {
-			reponse = 0;
-		}catch (ArithmeticException e){
-			System.out.println(e.getMessage());
-		}
+		System.out.println("une notation RPN c'est par exemple : (2*(1+2) qui va donner 212*+");
+		String reponse = console.nextLine();
 		
+		Stack<String> RPN = new Stack<String>();
+		while(RPN.isEmpty()) {
+			char[] decoupe = reponse.toCharArray();
+			for( int i = 0; i<decoupe.length;i++) {
+				if (reponse == "+") {
+					int a = Integer.parseInt(RPN.pop());
+					int b = Integer.parseInt(RPN.pop());
+					String c = String.valueOf(a+b);
+					RPN.push(c);					
+					}
+				else if (reponse == "*") {
+					int a = Integer.parseInt(RPN.pop());
+					int b = Integer.parseInt(RPN.pop());
+					String c = String.valueOf(a*b);
+					RPN.push(c);
+				}
+				else if (reponse == "-") {
+					int a = Integer.parseInt(RPN.pop());
+					int b = Integer.parseInt(RPN.pop());
+					String c = String.valueOf(a-b);
+					RPN.push(c);
+				}
+				else if (reponse == "/") {
+					int a = Integer.parseInt(RPN.pop());
+					int b = Integer.parseInt(RPN.pop());
+					String c = String.valueOf(a/b);
+					RPN.push(c);
+				}
+				else {
+					RPN.push(reponse);
+				}
+			System.out.println("Le résultat est donc :" + RPN);	
+			console.close();
+			}
+			
+		}
 	}
 }
+//https://www.delftstack.com/fr/howto/java/how-to-check-type-of-a-variable-in-java/
+//pb  https://codes-sources.commentcamarche.net/forum/affich-585561-decomposition-d-une-chaine-de-caractere
---
+++ poo/stack,queue/raw/exo5.java
@@ -0,0 +1,10 @@
+package exo_list;
+
+public class exo5 {
+
+	public static void main(String[] args) {
+		// https://www.baeldung.com/modulo-java
+
+	}
+
+}
